739e185e41b4c96f0e3556af32a85886370263b5
prabu766 <prabuddha.bhashitha@seera.sa>
2021-11-16 19:13:54 +0400

    adding single responsibility

---
+++ src/solid/principle/singleresponsibility/SingleResponsibility.java
@@ -0,0 +1,10 @@
+package solid.principle.singleresponsibility;
+
+/**
+ * 
+ * @author prabhuddha.bhashitha
+ * This class is to show Single Responsibility principle
+ */
+public class SingleResponsibility {
+
+}
